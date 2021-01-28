658c3bef7df70e61b99413ecede98bf8bdbe8ab8
xmmmmmovo <13256172360@163.com>
2021-01-28 23:00:06 +0800

    删除一些无用文件

---
+++ learn_os/rCore/target/riscv64imac-unknown-none-elf/debug/deps/rCore-ab950ccbdae4ec73.d
@@ -0,0 +1,5 @@
+/Users/xmmmmmovo/Documents/code/SandBearOS/rCore/target/riscv64imac-unknown-none-elf/debug/deps/rCore-ab950ccbdae4ec73.rmeta: src/main.rs
+
+/Users/xmmmmmovo/Documents/code/SandBearOS/rCore/target/riscv64imac-unknown-none-elf/debug/deps/rCore-ab950ccbdae4ec73.d: src/main.rs
+
+src/main.rs:

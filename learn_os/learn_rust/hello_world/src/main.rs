80829005443a75e22527000325e9c7882f66140e
xmmmmmovo <13256172360@163.com>
2021-01-30 22:05:17 +0800

    添加学习

---
+++ learn_os/learn_rust/hello_world/src/main.rs
@@ -0,0 +1,73 @@
+use std::fmt;
+use std::fmt::Formatter;
+
+fn main() {
+    // 通常情况下，`{}` 会被任意变量内容所替换。
+    // 变量内容会转化成字符串。
+    println!("{} days", 31);
+
+    // 不加后缀的话，31 就自动成为 i32 类型。
+    // 你可以添加后缀来改变 31 的类型。
+
+    // 用变量替换字符串有多种写法。
+    // 比如可以使用位置参数。
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    // 可以使用命名参数。
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+
+    // 可以在 `:` 后面指定特殊的格式。
+    println!("{} of {:b} people know binary, the other half don't", 1, 2);
+
+    // 你可以按指定宽度来右对齐文本。
+    // 下面语句输出 "     1"，5 个空格后面连着 1。
+    println!("{number:>width$}", number = 1, width = 6);
+
+    // 你可以在数字左边补 0。下面语句输出 "000001"。
+    println!("{number:>0width$}", number = 1, width = 6);
+
+    // println! 会检查使用到的参数数量是否正确。
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    // 改正 ^ 补上漏掉的参数："James"
+
+    // 创建一个包含单个 `i32` 的结构体（structure）。命名为 `Structure`。
+    #[allow(dead_code)]
+    // 从 `fmt::Debug` 获得实现给 `Structure`。
+    #[derive(Debug)]
+    struct Structure(i32);
+
+    impl fmt::Display for Structure {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    // 但是像结构体这样的自定义类型需要更复杂的方式来处理。
+    // 下面语句无法运行。
+    println!("This struct `{}` won't print...", Structure(3));
+
+    // 将 `Structure` 放到结构体 `Deep` 中。使 `Deep` 也能够打印。
+    #[derive(Debug)]
+    struct Deep(Structure);
+
+    // 打印操作使用 `{:?}` 和使用 `{}` 类似。
+    println!("{:?} months in a year.", 12);
+    println!(
+        "{1:?} {0:?} is the {actor:?} name.",
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
+
+    // `Structure` 是能够打印的类型。
+    println!("Now {:?} will print!", Structure(3));
+
+    // 使用 `derive` 的一个问题是不能控制输出的形式。
+    // 假如我只想展示一个 `7`？
+    println!("Now {:?} will print!", Deep(Structure(7)));
+}
--- learn_os/rCore-Tutorial/os/src/main.rs
+++ learn_os/rCore-Tutorial/os/src/main.rs
@@ -6,19 +6,32 @@
 //! - `#![no_main]`  
 //!   不使用 `main` 函数等全部 Rust-level 入口点来作为程序入口
 #![no_main]
+//! # 一些 unstable 的功能需要在 crate 层级声明后才可以使用
+//! - `#![feature(llvm_asm)]`  
+//!   内嵌汇编
+#![feature(llvm_asm)]
+//!
+//! - `#![feature(global_asm)]`  
+//!   内嵌整个汇编文件
+#![feature(global_asm)]
+//!
+//! - `#![feature(panic_info_message)]`  
+//!   panic! 时，获取其中的信息并打印
+#![feature(panic_info_message)]
 
-use core::panic::PanicInfo;
+#[macro_use]
+mod console;
+mod panic;
+mod sbi;
 
-/// 当 panic 发生时会调用该函数
-/// 我们暂时将它的实现为一个死循环
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+// 汇编编写的程序入口，具体见该文件
+global_asm!(include_str!("entry.asm"));
 
-/// 覆盖 crt0 中的 _start 函数
-/// 我们暂时将它的实现为一个死循环
+/// Rust 的入口函数
+///
+/// 在 `_start` 为我们进行了一系列准备之后，这是第一个被调用的 Rust 函数
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {}
+pub extern "C" fn rust_main() -> ! {
+    println!("Hello rCore-Tutorial!");
+    panic!("end of rust_main")
 }

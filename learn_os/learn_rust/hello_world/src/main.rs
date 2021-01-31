73d94b7dfd7c4c88edb0666c0857a02707118e46
xmmmmmovo <13256172360@163.com>
2021-01-31 09:01:05 +0800

    添加rust例子

--- learn_os/learn_rust/hello_world/src/main.rs
+++ learn_os/learn_rust/hello_world/src/main.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 use std::fmt::Formatter;
 
-fn main() {
+fn debug() {
     // 通常情况下，`{}` 会被任意变量内容所替换。
     // 变量内容会转化成字符串。
     println!("{} days", 31);
@@ -50,6 +50,14 @@ fn main() {
     // 但是像结构体这样的自定义类型需要更复杂的方式来处理。
     // 下面语句无法运行。
     println!("This struct `{}` won't print...", Structure(3));
+}
+
+fn display() {
+    // 创建一个包含单个 `i32` 的结构体（structure）。命名为 `Structure`。
+    #[allow(dead_code)]
+    // 从 `fmt::Debug` 获得实现给 `Structure`。
+    #[derive(Debug)]
+    struct Structure(i32);
 
     // 将 `Structure` 放到结构体 `Deep` 中。使 `Deep` 也能够打印。
     #[derive(Debug)]
@@ -70,4 +78,162 @@ fn main() {
     // 使用 `derive` 的一个问题是不能控制输出的形式。
     // 假如我只想展示一个 `7`？
     println!("Now {:?} will print!", Deep(Structure(7)));
+}
+
+fn display2() {
+    #[derive(Debug)]
+    struct MinMax(i64, i64);
+
+    impl fmt::Display for MinMax {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "({}, {})", self.0, self.1)
+        }
+    }
+
+    // 为了比较，定义一个含有具名字段的结构体。
+    #[derive(Debug)]
+    struct Point2D {
+        x: f64,
+        y: f64,
+    }
+
+    // 类似地对 `Point2D` 实现 `Display`
+    impl fmt::Display for Point2D {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // 自定义格式，使得仅显示 `x` 和 `y` 的值。
+            write!(f, "x: {}, y: {}", self.x, self.y)
+        }
+    }
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!(
+        "The big range is {big} and the small is {small}",
+        small = small_range,
+        big = big_range
+    );
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    // 报错。`Debug` 和 `Display` 都被实现了，但 `{:b}` 需要 `fmt::Binary`
+    // 得到实现。这语句不能运行。
+    // println!("What does Point2D look like in binary: {:b}?", point);
+}
+
+fn list() {
+    struct List(Vec<i32>);
+
+    impl fmt::Display for List {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            let vec = &self.0;
+            write!(f, "[")?;
+            for (count, v) in vec.iter().enumerate() {
+                if count != 0 {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{}", v)?;
+            }
+            write!(f, "]")
+        }
+    }
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+}
+
+fn format() {
+    struct City {
+        name: &'static str,
+        // 纬度
+        lat: f32,
+        // 经度
+        lon: f32,
+    }
+
+    impl fmt::Display for City {
+        // `f` 是一个缓冲区（buffer），此方法必须将格式化后的字符串写入其中
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+            let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+            // `write!` 和 `format!` 类似，但它会将格式化后的字符串写入
+            // 一个缓冲区（即第一个参数f）中。
+            write!(
+                f,
+                "{}: {:.3}°{} {:.3}°{}",
+                self.name,
+                self.lat.abs(),
+                lat_c,
+                self.lon.abs(),
+                lon_c
+            )
+        }
+    }
+
+    #[derive(Debug)]
+    struct Color {
+        red: u8,
+        green: u8,
+        blue: u8,
+    }
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *city);
+    }
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ]
+    .iter()
+    {
+        // 在添加了针对 fmt::Display 的实现后，请改用 {} 检验效果。
+        println!("{:?}", *color)
+    }
+}
+
+fn main() {
+    // debug();
+    // display();
+    // display2();
+    list();
 }

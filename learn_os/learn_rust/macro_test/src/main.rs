b611186aa2c93a9b86c36a26d79142d56ce366f8
xmmmmmovo <13256172360@163.com>
2021-02-06 16:11:42 +0800

    error 判定

--- learn_os/learn_rust/macro_test/src/main.rs
+++ learn_os/learn_rust/macro_test/src/main.rs
@@ -109,20 +109,20 @@ fn dsl_test() {
 
 fn mut_macro_test() {
     macro_rules! calculate {
-    // 单个 `eval` 的模式
-    (eval $e:expr) => {{
-        {
-            let val: usize = $e; // Force types to be integers
-            println!("{} = {}", stringify!{$e}, val);
-        }
-    }};
+        // 单个 `eval` 的模式
+        (eval $e:expr) => {{
+            {
+                let val: usize = $e; // Force types to be integers
+                println!("{} = {}", stringify!{$e}, val);
+            }
+        }};
 
-    // 递归地拆解多重的 `eval`
-    (eval $e:expr, $(eval $es:expr),+) => {{
-        calculate! { eval $e }
-        calculate! { $(eval $es),+ }
-    }};
-}
+        // 递归地拆解多重的 `eval`
+        (eval $e:expr, $(eval $es:expr),+) => {{
+            calculate! { eval $e }
+            calculate! { $(eval $es),+ }
+        }};
+    }
     calculate! { // 妈妈快看，可变参数的 `calculate!`！
         eval 1 + 2,
         eval 3 + 4,

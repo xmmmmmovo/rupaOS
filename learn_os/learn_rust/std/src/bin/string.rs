13bdbcae67579a716f52b6dc770588c93573f019
xmmmmmovo <13256172360@163.com>
2021-02-07 07:41:09 +0800

    通过例子学rust

---
+++ learn_os/learn_rust/std/src/bin/string.rs
@@ -0,0 +1,105 @@
+use std::str;
+
+fn main() {
+    // （所有的类型标注都不是必需的）
+    // 一个对只读内存中分配的字符串的引用
+    let pangram: &'static str = "the quick brown fox jumps over the lazy dog";
+    println!("Pangram: {}", pangram);
+
+    // 逆序迭代单词，这里并没有分配新的字符串
+    println!("Words in reverse");
+    for word in pangram.split_whitespace().rev() {
+        println!("> {}", word);
+    }
+
+    // 复制字符到一个 vector，排序并移除重复值
+    let mut chars: Vec<char> = pangram.chars().collect();
+    chars.sort();
+    chars.dedup();
+
+    // 创建一个空的且可增长的 `String`
+    let mut string = String::new();
+    for c in chars {
+        // 在字符串的尾部插入一个字符
+        string.push(c);
+        // 在字符串尾部插入一个字符串
+        string.push_str(", ");
+    }
+
+    // 这个缩短的字符串是原字符串的一个切片，所以没有执行新的分配操作
+    let chars_to_trim: &[char] = &[' ', ','];
+    let trimmed_str: &str = string.trim_matches(chars_to_trim);
+    println!("Used characters: {}", trimmed_str);
+
+    // 堆分配一个字符串
+    let alice = String::from("I like dogs");
+    // 分配新内存并存储修改过的字符串
+    let bob: String = alice.replace("dog", "cat");
+
+    println!("Alice says: {}", alice);
+    println!("Bob says: {}", bob);
+
+    // 通过转义，可以用十六进制值来表示字节。
+    let byte_escape = "I'm writing \x52\x75\x73\x74!";
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    // 也可以使用 Unicode 码位表示。
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!(
+        "Unicode character {} (U+211D) is called {}",
+        unicode_codepoint, character_name
+    );
+
+    let long_string = "String literals
+                        can span multiple lines.
+                        The linebreak and indentation here ->\
+                        <- can be escaped too!";
+    println!("{}", long_string);
+
+    let raw_str = r"Escapes don't work here: \x3F \u{211D}";
+    println!("{}", raw_str);
+
+    // 如果你要在原始字符串中写引号，请在两边加一对 #
+    let quotes = r#"And then I said: "There is no escape!""#;
+    println!("{}", quotes);
+
+    // 如果字符串中需要写 "#，那就在定界符中使用更多的 #。
+    // 可使用的 # 的数目没有限制。
+    let longer_delimiter = r###"A string with "# in it. And even "##!"###;
+    println!("{}", longer_delimiter);
+
+    // 注意这并不是一个 &str
+    let bytestring: &[u8; 20] = b"this is a bytestring";
+
+    // 字节串没有实现 Display，所以它们的打印功能有些受限
+    println!("A bytestring: {:?}", bytestring);
+
+    // 字节串可以使用单字节的转义字符...
+    let escaped = b"\x52\x75\x73\x74 as bytes";
+    // ...但不能使用 Unicode 转义字符
+    // let escaped = b"\u{211D} is not allowed";
+    println!("Some escaped bytes: {:?}", escaped);
+
+    // 原始字节串和原始字符串的写法一样
+    let raw_bytestring = br"\u{211D} is not escaped here";
+    println!("{:?}", raw_bytestring);
+
+    // 把字节串转换为 &str 可能失败
+    if let Ok(my_str) = str::from_utf8(raw_bytestring) {
+        println!("And the same as text: '{}'", my_str);
+    }
+
+    let quotes = br#"You can also use "fancier" formatting, \
+                    like with normal raw strings"#;
+
+    // 字节串可以不使用 UTF-8 编码
+    let shift_jis = b"\x82\xe6\x82\xa8\x82\xb1\x82"; // SHIFT-JIS 编码的 "ようこそ"
+
+    // 但这样的话它们就无法转换成 &str 了
+    match str::from_utf8(shift_jis) {
+        Ok(my_str) => println!("Conversion successful: '{}'", my_str),
+        Err(e) => println!("Conversion failed: {:?}", e),
+    };
+}

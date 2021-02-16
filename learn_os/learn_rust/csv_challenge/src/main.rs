e0e338bcb47b8f365c0a7ce28f740dfe1747e95b
xmmmmmovo <13256172360@163.com>
2021-02-16 22:23:46 +0800

    append rust unsafe

--- learn_os/learn_rust/csv_challenge/src/main.rs
+++ learn_os/learn_rust/csv_challenge/src/main.rs
@@ -1,10 +1,37 @@
-use crate::opt::Opt;
+use csv_challenge::{
+    replace_column, Opt, {load_csv, write_csv},
+};
+use std::path::PathBuf;
+use std::process;
 use structopt::StructOpt;
-mod opt;
-mod err;
-mod core;
 
 fn main() {
     let opt = Opt::from_args();
-    println!("{:?}", opt);
+    let filename = PathBuf::from(opt.input);
+    let csv_data = match load_csv(filename) {
+        Ok(fname) => fname,
+        Err(e) => {
+            println!("main error: {:?}", e);
+            process::exit(1);
+        }
+    };
+
+    let modified_data = match replace_column(csv_data, &opt.column_name, &opt.replacement) {
+        Ok(data) => data,
+        Err(e) => {
+            println!("main error: {:?}", e);
+            process::exit(1);
+        }
+    };
+
+    let output_file = &opt.output.unwrap_or("output/output.csv".to_string());
+    match write_csv(&modified_data, &output_file) {
+        Ok(_) => {
+            println!("write success!");
+        }
+        Err(e) => {
+            println!("main error: {:?}", e);
+            process::exit(1);
+        }
+    }
 }
